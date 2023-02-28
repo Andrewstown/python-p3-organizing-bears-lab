6db81131a8d533268d28b5b343e6cfa09ec2a2d4
+ <stingeyandrewstown@live.com>
2023-02-28 12:43:06 -0600

    GAY

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 24, 'M', 'Brown', 'Mellow', 1),
+('Rowdy', 16, 'M', 'White Chocolate', 'Rowdy', 1),
+('Tabitha', 25, 'F', 'Rose Gold', 'Caring', 1),
+('Sergeant Brown', 62, 'M', 'Black', 'Hearty', 0),
+('Melissa', 7, 'F', 'Light Yellow', 'Cheery', 1),
+('Grinch', 100, 'M', 'Green', 'Naughty', 1),
+('Wendy', 64, 'F', 'Red', 'Helpful', 0),
+(NULL, 64, 'F', 'Red', 'Helpful', 0);
